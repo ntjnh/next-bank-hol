15ed49a61a4877c005cd10096644b5736ebf38c4
Nate J.M <nate@natejonah.com>
2023-08-01 13:25:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Next UK Bank Holiday
+
+ Just a simple React app that tells you when the next bank holiday is in the UK. The data comes from the [GOV API](https://www.api.gov.uk/gds/bank-holidays/#bank-holidays).
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <header>
+                <h1>Vite + React</h1>
+            </header>
+
+            <main>
+                <h2>body</h2>
+            </main>
+
+            <footer>
+                <a href="https://natejonah.com" target="_blank">Nate</a> built this.
+            </footer>
+        </>
+    )
+}
+
+export default App
