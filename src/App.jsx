a8db430404998552d4fb934ee2447f88cd5604dc
Nate J.M <nate@natejonah.com>
2023-08-05 13:53:42 +0100

    Add basic header and footer styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import Country from './components/Country'
 function App() {
     return (
         <>
-            <header>
-                <h1>Next Bank Holiday</h1>
+            <header className='border-b mb-5 px-6 py-8 text-center'>
+                <h1 className='font-semibold text-3xl'>Next Bank Holiday</h1>
             </header>
 
             <main>
                 <Country />
             </main>
 
-            <footer>
-                <a href="https://natejonah.com?utm_source=codebynate&utm_medium=web&utm_campaign=next_bank_hol" target="_blank" rel="noreferrer">Nate</a> built this.
+            <footer className='border-t mt-5 px-6 py-4 text-center text-xs'>
+                <a className='text-sky-600 hover:text-sky-800' href='https://natejonah.com?utm_source=codebynate&utm_medium=web&utm_campaign=next_bank_hol' target='_blank' rel='noreferrer'>Nate</a> built this.
             </footer>
         </>
     )
