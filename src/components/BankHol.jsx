4133ab989a9ebde71c13fa38151084e417bd7edc
Nate J.M <nate@natejonah.com>
2023-08-06 18:06:29 +0100

    Begin styling BankHol component

--- src/components/BankHol.jsx
+++ src/components/BankHol.jsx
@@ -1,6 +1,6 @@
 export default function BankHol(props) {
     return (
-        <div>
+        <div className='bg-zinc-900 border border-zinc-700/40 max-w-full px-2 py-4 text-center text-zinc-100'>
             <h2>The next bank holiday in {props.country} is</h2>
             <h3>{props.bankHolDate}</h3>
             <h4>{props.bankHolName}</h4>
