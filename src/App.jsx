11c18d0a864645864f404bcfc873e1c1a962aad5
Nate J.M <nate@natejonah.com>
2023-08-06 18:06:07 +0100

    Add more style to header, footer and main

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import Country from './components/Country'
 
 function App() {
     return (
-        <>
-            <header className='border-b mb-5 px-6 py-8 text-center'>
-                <h1 className='font-semibold text-3xl'>Next Bank Holiday</h1>
+        <div className='bg-black flex flex-col flex-wrap items-start min-h-screen'>
+            <header className='border-b border-zinc-700/70 px-6 py-8 text-center w-full'>
+                <h1 className='font-semibold text-3xl text-zinc-100'>Next Bank Holiday</h1>
             </header>
 
-            <main>
+            <main className='flex-1 px-6 py-8 self-stretch w-full'>
                 <Country />
             </main>
 
-            <footer className='border-t mt-5 px-6 py-4 text-center text-xs'>
+            <footer className='border-t border-zinc-700/70 px-6 py-4 self-end text-center text-xs text-zinc-600 w-full'>
                 <a className='text-sky-600 hover:text-sky-800' href='https://natejonah.com?utm_source=codebynate&utm_medium=web&utm_campaign=next_bank_hol' target='_blank' rel='noreferrer'>Nate</a> built this.
             </footer>
-        </>
+        </div>
     )
 }
 
