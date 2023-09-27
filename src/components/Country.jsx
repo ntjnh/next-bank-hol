32817ea2d1f92a2c5998cf60bbfc6372ed784589
Nate J.M <nate@natejonah.com>
2023-09-27 22:48:50 +0100

    Fix active tab issue and clean up component code

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -4,11 +4,12 @@ import filterEvents from '../functions/filterEvents'
 import monthToString from '../functions/monthToString'
 
 function Country() {
-    // Create state for bank hol data
+    const countries = ['England and Wales', 'Scotland', 'Northern Ireland']
+
     const [bankHols, setBankHols] = useState({})
     const [selectedCountry, setSelectedCountry] = useState({})
-    const [isActive, setIsActive] = useState(false)
-    const [selectedTab, setSelectedTab] = useState(null)
+
+    const [activeTab, setActiveTab] = useState(countries[0])
     
     // Grab dates from API
     useEffect(() => {
@@ -24,7 +25,7 @@ function Country() {
                 const engWalesNextEvent = filterEvents(data['england-and-wales'].events)[0]
                 const engWalesNextEventDate = engWalesNextEvent.date
             
-                // Create state for the current country
+                // Create state for the current country and set England and Wales as the default
                 setSelectedCountry({
                     full: 'England and Wales',
                     name: engWalesNextEvent.title,
@@ -36,7 +37,6 @@ function Country() {
 
 
     function getNextBankHol(country) {
-        // Set England and Wales as the default
         const currentCountry = country.toLowerCase().replace(/\s/g, '-')
 
         // Get dates for the country and filter them
@@ -53,37 +53,23 @@ function Country() {
             date: `${nextBankHolDate.getDate()} ${monthToString(nextBankHolDate)}`,
             activeDefault: false
         })
-    }    
-
-    function toggleClasses(selectedTabId) {
-        setIsActive(selectedTab === selectedTabId)
     }
 
     const allTabClasses = 'inline-flex items-center justify-center w-full h-14 gap-2 px-4 -mb-px text-gray-400 text-sm font-medium tracking-wide transition duration-300 focus-visible:outline-none disabled:cursor-not-allowed'
-    const activeTabClasses = 'text-sky-700 focus:text-sky-700 stroke-sky-500 hover:stroke-sky-600 focus:stroke-sky-700'
-    const genericTabClasses = 'justify-self-center text-gray-600 stroke-slate-700 focus:stroke-sky-600 focus:text-sky-600 hover:stroke-sky-600'
-
-    const tabClasses = `${allTabClasses} ${(isActive) ? activeTabClasses : genericTabClasses}`
+    const activeTabClasses = `${allTabClasses} bg-sky-700 focus:bg-sky-700 text-white focus:text-white stroke-sky-500 hover:stroke-sky-700 focus:stroke-sky-700`
+    const genericTabClasses = `${allTabClasses} justify-self-center text-gray-600 stroke-slate-700 hover:stroke-sky-600`
 
     // Update data each time a tab is clicked depending on which country is active
-    function tabClick(e) {
-        // Get name of country that's been clicked
-        const clickedCountry = e.target.parentNode.dataset.country
-
-        let selectedTabId = e.target.id
-        let selected = selectedTab === selectedTabId ? null : selectedTabId
-
-        setSelectedTab(selected)
-        toggleClasses(selectedTabId)
-
-        getNextBankHol(clickedCountry)
+    function handleClick(country) {
+        setActiveTab(country)
+        getNextBankHol(country)
     }
 
     // Full country names for display on front end
-    const countryNames = ['England and Wales', 'Scotland', 'Northern Ireland'].map((countryName, i) => {
+    const countryNames = countries.map((countryName, i) => {
         return (
-            <li key={i} className='w-1/3' data-country={countryName} role='presentation' onClick={tabClick}>
-                <button className={(i == 0 && selectedCountry.activeDefault) ? `${allTabClasses} ${activeTabClasses}` : tabClasses} id={`tab-${i}`} role='tab' aria-setsize='3' aria-posinset={i+1} tabIndex={isActive ? '0' : '-1'} aria-controls={`tab-panel-${i+1}`} aria-selected={isActive ? 'true' : 'false'}>{countryName}</button>
+            <li key={i} className='w-1/3' data-country={countryName} role='presentation'>
+                <button onClick={() => handleClick(countryName)} className={activeTab === countryName ? activeTabClasses : genericTabClasses} id={`tab-${i}`} role='tab' aria-setsize='3' aria-posinset={i+1} tabIndex={activeTab === countryName ? '0' : '-1'} aria-controls={`tab-panel-${i+1}`} aria-selected={activeTab === countryName ? 'true' : 'false'}>{countryName}</button>
             </li>
         )
     })
