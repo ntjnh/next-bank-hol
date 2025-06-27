b623557b8ad4d49819190f22d8122843966bdb30
Nate M <codebynate@gmail.com>
2025-06-27 17:08:46 +0100

    Create list component for upcoming dates

---
+++ src/components/List.jsx
@@ -0,0 +1,16 @@
+import Table from './Table'
+
+export default function List({ country, dates }) {
+    return (
+        <div className="max-w-screen-sm mx-auto py-7">
+            <h2 className="font-bold mb-5 text-xl">
+                Upcoming bank holidays in {country}
+            </h2>
+
+            {dates.map((date, i) => {
+                let year = date[0].date
+                return <Table key={i} holidays={date} year={year.getFullYear()} />
+            })}
+        </div>
+    )
+}
