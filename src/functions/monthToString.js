ab06a2c670c125abbf3174c3900a6f818fe6e5ba
Nate M <codebynate@gmail.com>
2025-06-27 17:07:37 +0100

    Create dayToString function

--- src/functions/monthToString.js
+++ src/functions/monthToString.js
@@ -1,8 +1,17 @@
 // Convert zero-based version of month to text string
-const monthToString = date => {
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-    return months[date.getMonth()]
+export default function monthToString(date) {
+    return [
+        'January', 
+        'February', 
+        'March', 
+        'April', 
+        'May', 
+        'June', 
+        'July', 
+        'August', 
+        'September', 
+        'October', 
+        'November', 
+        'December'
+    ][date.getMonth()]
 }
-
-export default monthToString
